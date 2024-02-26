3708d4ea1121a1b73e074a44e9773ebf0485b7cc
Sreeshma Tharammal <sreeshmajeenus@gmail.com>
2024-02-26 00:17:16 +0000

    Add support to get all user names

--- run.py
+++ run.py
@@ -12,3 +12,17 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('number-ninja')
 
+USER_RECORDS_WORKSHEET = SHEET.worksheet("user_records")
+
+def get_all_user_name():
+    """ Get all user names """
+        
+    users = USER_RECORDS_WORKSHEET.col_values(1)[1:]
+    return users
+
+def main():
+    """ Run all program function """
+    users = get_all_user_name()    
+    print(users)
+
+main()
